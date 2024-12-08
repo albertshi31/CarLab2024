7ab50bed9bc8b93d84958ad68f925fb2654a90c9
Albert Shi <as6369@princeton.edu>
2024-12-08 00:44:37 -0500

    hi

--- all_motors.py
+++ all_motors.py
@@ -12,7 +12,7 @@ motors = {
 
 # Adjust this value for smoother operation
 step_sleep = 0.01
-step_count = 4096  # Adjust based on- motor specs
+step_count = 4096  # Adjust based on motor specs
 
 # Step sequence
 step_sequence = [[1, 0, 0, 1],
@@ -39,19 +39,23 @@ def cleanup():
     GPIO.cleanup()
 
 # Motor control function
-def drive_motor(motor_id, direction):
+def drive_motor(motor_id):
     motor_pins = motors[motor_id]['pins']
     motor_step_counter = 0
+    direction = False  # False for counter-clockwise, True for clockwise
 
     try:
-        for i in range(step_count):
-            for pin in range(0, len(motor_pins)):
-                GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
-            if direction:
-                motor_step_counter = (motor_step_counter - 1) % 8
-            else:
-                motor_step_counter = (motor_step_counter + 1) % 8
-            time.sleep(step_sleep)
+        while True:  # Run indefinitely
+            for i in range(step_count):
+                for pin in range(0, len(motor_pins)):
+                    GPIO.output(motor_pins[pin], step_sequence[motor_step_counter][pin])
+                if direction:
+                    motor_step_counter = (motor_step_counter - 1) % 8
+                else:
+                    motor_step_counter = (motor_step_counter + 1) % 8
+                time.sleep(step_sleep)
+            # Reverse direction after completing step_count steps
+            direction = not direction
     except KeyboardInterrupt:
         cleanup()
         exit(1)
@@ -59,7 +63,7 @@ def drive_motor(motor_id, direction):
 # Start threads for each motor
 threads = []
 for motor_id in motors.keys():
-    t = threading.Thread(target=drive_motor, args=(motor_id, False))  # False for counter-clockwise
+    t = threading.Thread(target=drive_motor, args=(motor_id,))
     threads.append(t)
     t.start()
 
