733b434d3e3f09aba0bb969b0d98547448e77bed
Christine Guo <cg4302@princeton.edu>
2024-12-10 00:06:23 -0500

    new file

--- voice_command.py
+++ voice_command.py
@@ -73,7 +73,7 @@ def recognize_command():
             print("Starting laser detection.")
             # Run the laser detection script with sudo
             laser_process = subprocess.Popen(['sudo', 'python3', 'detect_green.py'])  # Start laser tracking
-            leg_process = subprocess.Popen(['sudo', 'python3', 'all_motors.py']) 
+            leg_process = subprocess.Popen(['python3', 'all_motors.py']) 
             laser_detection_running = True
 
         elif 'stop' in command and laser_detection_running:
